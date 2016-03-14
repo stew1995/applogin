bf8465c899b6d9cced5a2747f2d2d306e045261f
stew1995 <up703451@myport.ac.uk>
2016-03-14 16:12:56 +0000

    Update registersidebar.php

--- pages/registersidebar.php
+++ pages/registersidebar.php
@@ -9,6 +9,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $nameErr = "Name is required";
   } else {
     $name = test_input($_POST["FName"]);
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+  $nameErr = "Only letters and white space allowed"; 
+}
   }
 
   if(empty($_POST["LName"])) {
