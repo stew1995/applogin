d9c64b2a5d89d3709e9e97eff211ea792a086b06
stew1995 <up703451@myport.ac.uk>
2016-03-14 16:03:53 +0000

    Update registersidebar.php
    Added PHP code to page to see if works

--- pages/registersidebar.php
+++ pages/registersidebar.php
@@ -1,3 +1,69 @@
+        <?php
+//define variables and set to empty values
+$name = $surname = $DOB = $email = $address = $postcode = $homeNumber = $mobile = "";
+//defined error variables
+$nameErr = $surnameErr = $DOBErr = $emailErr = $addressErr = $postcodeErr = $mobileErr = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  if(empty($_POST["FName"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["FName"]);
+  }
+
+  if(empty($_POST["LName"])) {
+    $surnameErr = "Surname is required";
+  } else {
+    $surname = test_input($_POST["LName"]);
+  }
+
+  if(empty($_POST["DOB"])) {
+    $DOBErr = "Date of Birth is required";
+  } else {
+    $DOB = test_input($_POST["DOB"]);
+  }
+
+  if(empty($_POST["Email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["Email"]);
+  }
+
+  if(empty($_POST["HomeAddress"])) {
+    $addressErr = "Address is required";
+  } else {
+    $address = test_input($_POST["HomeAddress"]);
+  }
+
+  if(empty($_POST["Postcode"])) {
+    $postcodeErr = "Postcode is required";
+  } else {
+    $postcode = test_input($_POST["Postcode"]);
+  }
+
+  if(empty($_POST["HomeNumber"])) {
+    $homeNumber = "";
+  } else {
+    $homeNumber = test_input($_POST["HomeNumber"]);
+  }
+}
+
+if(empty($_POST["Mobile"])) {
+  $mobileErr = "Mobile number is required";
+} else {
+  $mobile = test_input($_POST["Mobile"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+        
+        
         <form class="registrationForm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             <h1>Register</h1>
             <p>Enter some basic information about yourself</p>
@@ -20,4 +86,6 @@
           echo $name;
           echo $surname;
           echo $email;
+          echo $DOB;
+          echo $address;
           ?>
