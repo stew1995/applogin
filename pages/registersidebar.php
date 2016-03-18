3fa82de7aed02c6bd7a8d83940413645b13bd07d
stew1995 <up703451@myport.ac.uk>
2016-03-18 13:28:18 +0000

    Update registersidebar.php

--- pages/registersidebar.php
+++ pages/registersidebar.php
@@ -1,3 +1,19 @@
+        <form class="registrationForm" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <h1>Register</h1>
+            <p>Enter some basic information about yourself</p>
+
+            <p><span class="required">* required field.</span></p>
+
+            <p>First Name<input type="text" name="FName"><span class="required">*<?php echo $nameErr;?></span></p>
+            <p>Last Name<input type="text" name="LName"><span class="required">*<?php echo $nameErr;?></span></p>
+            <p>Date Of Birth<input type="text" name="DOB" maxlength="9" size ="9"><span class="required">*<?php echo $nameErr;?></span></p>
+            <p>Email<input type="text" name="Email"><span class="required">*<?php echo $nameErr;?></span></p>
+            <p>Home Address<input type="text" name="HomeAddress"><span class="required">*<?php echo $nameErr;?></span></p>
+            <p>Postcode<input type="text" name="Postcode" maxlength="7" size="7"><span class="required">*<?php echo $nameErr;?></span></p>
+            <p>Home Number<input type="number" name="HomeNumber"></p>
+            <p>Mobile<input type="number" name="Mobile"><span class="required">*<?php echo $nameErr;?></span></p>
+            <p><input type="submit" name="completeBasic" value="Submit"></p>
+          </form>
 <?php
 //define variables and set to empty values
 $name = $surname = $DOB = $email = $address = $postcode = $homeNumber = $mobile = "";
@@ -64,19 +80,3 @@ function test_input($data) {
   return $data;
 }
 ?>
-        <form class="registrationForm" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-            <h1>Register</h1>
-            <p>Enter some basic information about yourself</p>
-
-            <p><span class="required">* required field.</span></p>
-
-            <p>First Name<input type="text" name="FName"><span class="required">*<?php echo $nameErr;?></span></p>
-            <p>Last Name<input type="text" name="LName"><span class="required">*<?php echo $nameErr;?></span></p>
-            <p>Date Of Birth<input type="text" name="DOB" maxlength="9" size ="9"><span class="required">*<?php echo $nameErr;?></span></p>
-            <p>Email<input type="text" name="Email"><span class="required">*<?php echo $nameErr;?></span></p>
-            <p>Home Address<input type="text" name="HomeAddress"><span class="required">*<?php echo $nameErr;?></span></p>
-            <p>Postcode<input type="text" name="Postcode" maxlength="7" size="7"><span class="required">*<?php echo $nameErr;?></span></p>
-            <p>Home Number<input type="number" name="HomeNumber"></p>
-            <p>Mobile<input type="number" name="Mobile"><span class="required">*<?php echo $nameErr;?></span></p>
-            <p><input type="submit" name="completeBasic" value="Submit"></p>
-          </form>
