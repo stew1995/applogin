ba1647ff8f3aced9d8acb73f7e3eb4cec715e299
stew1995 <up703451@myport.ac.uk>
2016-03-31 12:02:23 +0100

    Update data.php

--- lib/data.php
+++ lib/data.php
@@ -8,7 +8,7 @@ $dbname = "web";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 //Check connection
 if($conn) {
-  die("Connection to database failed". mysqli_error($conn));
+  die("Connection to database failed". mysqli_connect_error($conn));
 }
 
 //SQL to Create tables
