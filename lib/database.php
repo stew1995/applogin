32734c28a8d78ca92304aed1a11dc68e8979666c
stew1995 <up703451@myport.ac.uk>
2016-03-31 11:22:49 +0100

    Create database.php

---
+++ lib/database.php
@@ -0,0 +1,23 @@
+<?php
+
+$servername = "localhost";
+$username = "up703451";
+$password = "amber1995";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+//Check connection
+if(!$conn) {
+  die("Connection to database failed: ". mysqli_connect_error());
+}
+
+//Create database
+$sql = "CREATE DATABASE web";
+if(mysqli_query($conn, $sql)) {
+  echo "Database was succesfully created";
+} else {
+  echo "Error creating database: ". mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
