9aacefcee72b1cb5e51072e1bee233171e8fc6a6
stew1995 <up703451@myport.ac.uk>
2016-04-07 12:53:44 +0100

    Update and rename supportrightbar.txt to supportrightbar.php
    Added support content and chnaged to php

---
+++ pages/supportrightbar.php
@@ -0,0 +1,50 @@
+<div class="supportright">
+ <h1>Contact Information</h1>
+   <h2>Univeristy contacts</h2>
+     <h3 id="SU"><span id="textAlign">Student Union</h3></span>
+       <div id="contactBackground">
+         <span id="SUContent"><p>Cambridge Road, Portsmouth, Hampshire, PO1 2ST</p>
+         <p>023 9284 3628</p></span>
+       </div>
+     <h3 id="UL"><span id="textAlign">University Library</h3></span>
+       <div id="contactBackground">
+         <span id="ULContent"><p>The University Library, Cambridge Rd, Portsmouth, Hampshire, PO1 2ST</p>
+         <p>023 9284 3228</p></span>
+       </div>
+     <h3 id="UD"><span id="textAlign">University Doctors</h3></span>
+       <div id="contactBackground">
+         <span id="UDContent"><p>The Nuffield Centre, St Michael's Rd, Portsmouth,Hampshire, PO1 2BH</p>
+         <p>023 9273 6006</p></span>
+       </div>
+     <h3 id="PD"><span id="textAlign">Purple Door</h3></span>
+       <div id="contactBackground">
+         <span id="PDContent"><p>28 Guildhall Walk, Portsmouth, PO1 2DD</p>
+         <p>023 9284 2684</p>
+         <p>purpledoor@port.ac.uk</p></span>
+       </div>
+     <h3 id="SH"><span id="textAlign">Student Halls</h3></span>
+       <div id="contactBackground">
+         <span id="SHContent"><select id="contactStudentHalls">
+                                 <option>James Watson</option>
+                                 <option>Margrete Rule</option>
+                                 <option>Langstone</option>
+                                 <option>Bateson</option>
+                                 <option>Harry Law</option>
+                                 <option>Rees Hall</option>
+                                 <option>Trafalgar Hall</option>
+                             </select>
+       </span>
+       </div>
+     <h3 id="Secur"><span id="textAlign">Security</h3></span>
+       <div id="contactBackground">
+         <span id="SecurContent"><p>023 9284 3333 - Emergency</p>
+         <p>023 9284 3418 - Control Room</p>
+         <p>campus.security@port.ac.uk</p></span>
+       </div>
+   <h3 id="Estate"><span id="textAlign">Estate Agents</h3></span>
+     <div id="contactBackground">
+       <span id="EstateContent"><h3>Student Pad</h3>
+       <p>studentpad</p></span>
+     </div>
+</div>
+
