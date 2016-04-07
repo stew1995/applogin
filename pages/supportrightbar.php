0005f9729749041179135bd09825ce6ed2da0e70
stew1995 <up703451@myport.ac.uk>
2016-04-07 14:10:46 +0100

    Update supportrightbar.php

--- pages/supportrightbar.php
+++ pages/supportrightbar.php
@@ -50,6 +50,6 @@
        <p>studentpad</p></span>
      </div>
 </div>
-<?php echo '<script src="javascript/support.js"></script>'; ?>
+<script src="javascript/support.js"></script>
  </body>
 </html>
