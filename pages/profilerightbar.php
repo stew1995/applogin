82ade1999681a3ba20291686c420262c5206a039
stew1995 <up703451@myport.ac.uk>
2016-04-11 13:24:45 +0100

    Update profilerightbar.php

--- pages/profilerightbar.php
+++ pages/profilerightbar.php
@@ -1,7 +1,34 @@
 <!DOCTYPE html>
 <html>
 </body>
-</html><div class="profileRight">
+          <?php
+          if(isset($_POST)){
+            if($_POST['submitSave'] == "Create Note" && !empty($_POST['noteName'])){
+              if(!file_exists($_POST['noteName'] . ".txt")){
+                $file = tmpfile();
+              }
+              $file = fopen($_POST['noteName'] . ".txt", "a+");
+              while(!feof($file)){
+                $old = $old . "<p>" . fgets($file) . "</p>";
+            }
+            $text = $_POST["noteData"];
+            file_put_contents($_POST['noteName'] . ".txt" . $old . $text);
+            fclose($file);
+          }
+          
+            if($_POST['submitOpen'] == "Load Note") {
+              if(!file_exists($_POST['noteName'] . ".txt")){
+                exit("Error: Note does not exist");
+              }
+          
+              $file = fopen($_POST['noteName'] . ".txt", "r");
+              while(!feof($file)){
+                echo "<p>" . fgets($file) . "</p>";
+              }
+              fclose($file);
+            }
+          ?>
+<div class="profileRight">
           <h3>Housing group</h3>
           <p style="text-decoration: underline;">Group Name:
             <form name="groupCreate">
@@ -39,33 +66,6 @@
               <p>Open Note: <input type="text" name="noteName" value=""></p>
               <p><input type="button" name="submitOpen" value="Load Note"></p>
             </form>
-          <?php
-          if(isset($_POST)){
-            if($_POST['submitSave'] == "Create Note" && !empty($_POST['noteName'])){
-              if(!file_exists($_POST['noteName'] . ".txt")){
-                $file = tmpfile();
-              }
-              $file = fopen($_POST['noteName'] . ".txt", "a+");
-              while(!feof($file)){
-                $old = $old . "<p>" . fgets($file) . "</p>";
-            }
-            $text = $_POST["noteData"];
-            file_put_contents($_POST['noteName'] . ".txt" . $old . $text);
-            fclose($file);
-          }
-          
-            if($_POST['submitOpen'] == "Load Note") {
-              if(!file_exists($_POST['noteName'] . ".txt")){
-                exit("Error: Note does not exist");
-              }
-          
-              $file = fopen($_POST['noteName'] . ".txt", "r");
-              while(!feof($file)){
-                echo "<p>" . fgets($file) . "</p>";
-              }
-              fclose($file);
-            }
-          ?>
           <div id="notesContent"> </div>
 
         </div>
