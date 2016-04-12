00d92cead555470a3d63b6636a0e482bc6558de7
stew1995 <up703451@myport.ac.uk>
2016-04-12 21:07:51 +0100

    Create createGroup.php

---
+++ lib/createGroup.php
@@ -0,0 +1,36 @@
+<?php
+if(isset($_POST['submitCreateGroup'])) {
+  //Database Variables
+  $servername = "localhost";
+  $username = "root";
+  $password = "root";
+  $dbname = "web";
+  //Create connection
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+  //Check connection
+  if(!$conn) {
+    die("Connection to database failed: ". mysqli_connect_error());
+  }
+
+  if(!get_magic_quotes_gpc()) {
+    $groupName = addslashes($_POST['createGroupName']);
+    $groupDesc = addslashes($_POST['groupDescription']);
+    //$addUser = addslashes($_POST['addToGroup']);
+  } else {
+    $groupName = $_POST['createGroupName'];
+    $groupDesc = $_POST['groupDescription'];
+    //$addUser = $_POST['addToGroup'];
+  }
+
+  $sql = "INSERT INTO house (name, description) VALUES('$groupName', '$groupDesc')";
+  mysqli_select_db('web');
+  $retval = mysqli_query($sql, $conn);
+
+  if(!$retval) {
+    die('Could not enter data: ' . mysqli_connect_error());
+  }
+
+  echo "Data entered successfully\n";
+  mysqli_close($conn);
+}
+ ?>
