c722ca4342425a59a96abe84c705239bce16db79
stew1995 <up703451@myport.ac.uk>
2016-04-28 13:34:38 +0100

    Update data.php

--- lib/data.php
+++ lib/data.php
@@ -430,9 +430,12 @@ mysqli_query($conn, $sqlunidata) or die(mysqli_error($conn). 'flat');
 
 //Data for house table
 $sqlhousedata =
-"INSERT IGNORE INTO `house` ('group_id','user_id','name','description') VALUES (1,3, 'House Group', 'Looking for a house'),
-(2,6, 'Next Year House', 'Need to find a house for next year'),(2,8,'Next Year House', 'Need to find a house for next year'),
-(1,15, 'House Group', 'Looking for a house'),(2,16,'Next Year House', 'Need to find a house for next year')";
+"INSERT IGNORE INTO `house` (`user_id`, `group_id`, `name`, `description`) VALUES
+(3,1, 'House Group', 'Looking for a house'),
+(6,2, 'Next Year House', 'Need to find a house for next year'),
+(8,2,'Next Year House', 'Need to find a house for next year'),
+(15,1, 'House Group', 'Looking for a house'),
+(16,2,'Next Year House', 'Need to find a house for next year')";
 
 mysqli_query($conn, $sqlhousedata) or die(mysqli_error($conn). 'house');
 
