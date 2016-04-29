bc473ab507f12fa7d7c89d0983375da0b339697c
stew1995 <up703451@myport.ac.uk>
2016-04-29 13:24:36 +0100

    Update searchexec.php

--- lib/searchexec.php
+++ lib/searchexec.php
@@ -1,6 +1,15 @@
 <?php
-include('conn.php');
-if(isset($_GET['search']) && $_GET['search'] != '') {
+ini_set('display_errors', FALSE);
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "web";
+//Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+//Check connection
+if(!$conn) {
+  die("Connection to database failed: ". mysqli_connect_error());
+}if(isset($_GET['search']) && $_GET['search'] != '') {
   $search = $_GET['search'];
   $result = mysqli_query($conn, "SELECT * FROM user WHERE fname LIKE ('%".$search."%')");
   while($row = mysqli_fetch_array($result)) {
