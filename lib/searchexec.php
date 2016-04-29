d2ae437d86edbd3e5cfad3cbe4bb5b23a418a20c
stew1995 <up703451@myport.ac.uk>
2016-04-29 13:22:05 +0100

    Update searchexec.php

--- lib/searchexec.php
+++ lib/searchexec.php
@@ -2,7 +2,7 @@
 include('conn.php');
 if(isset($_GET['search']) && $_GET['search'] != '') {
   $search = $_GET['search'];
-  $result = mysqli_query("SELECT * FROM user WHERE fname LIKE ('%".$search."%')");
+  $result = mysqli_query($conn, "SELECT * FROM user WHERE fname LIKE ('%".$search."%')");
   while($row = mysqli_fetch_array($result)) {
     echo ''. $row['fname'] . $row['lname'] . "\n";
   }
