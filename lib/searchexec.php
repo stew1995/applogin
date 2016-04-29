9a42bc2dc185d7044cfbed501de99cd79c7d73d0
stew1995 <up703451@myport.ac.uk>
2016-04-29 20:20:14 +0100

    Update searchexec.php
    UPDATED SQL QUERY AND ADDED TABLE

--- lib/searchexec.php
+++ lib/searchexec.php
@@ -11,7 +11,12 @@ if(!$conn) {
   die("Connection to database failed: ". mysqli_connect_error());
 }if(isset($_GET['search']) && $_GET['search'] != '') {
   $search = $_GET['search'];
-  $result = mysqli_query($conn, "SELECT * FROM user WHERE fname LIKE ('%".$search."%')");
+  //$result = mysqli_query($conn, "SELECT * FROM user WHERE fname LIKE ('%".$search."%')");
+  $result = mysqli_query($conn, "SELECT fname, lname, accom, study 
+                                FROM user  
+                                INNER JOIN uni  
+                                ON user.id = uni.user_id 
+                                WHERE fname LIKE ('%".$search."%')");
   echo "<table id='searchresulttable'";
     echo "<tr>";
       echo "<th>Name</th>";
@@ -21,9 +26,11 @@ if(!$conn) {
   while($row = mysqli_fetch_array($result)) {
     echo "<tr>";
       echo "<td>" . $row['fname']. " ". $row['lname'] . "</td>";
-      echo "<td>" . $row['']
-      
-      echo ''. $row['fname'] . $row['lname'] . "\n";
+      echo "<td>" . $row['accom']. "</td>";
+      echo "<td>" . $row['study']. "</td>";
+    echo "</tr>";
+      //echo ''. $row['fname'] . " ". $row['lname'] . "\n";
   }
+  echo "</table>";
 }
 ?>
