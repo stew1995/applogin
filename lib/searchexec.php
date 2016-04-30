108e1500480a454d8f7bae1e43a7a4dfdde6fc47
stew1995 <up703451@myport.ac.uk>
2016-04-30 15:25:38 +0100

    Update searchexec.php

--- lib/searchexec.php
+++ lib/searchexec.php
@@ -73,11 +73,11 @@ if(!$conn) {
     echo "</tr>";
   while($row = mysqli_fetch_array($result)) {
      echo ''. "<tr>";
-   echo "<td>". $row['fname']. " ". $row['lname'] . "</td>" . "\n";
-     echo "<td>". $row['course'] . "</td>" . "\n";
-     echo "<td>". $row['accom'] . "</td>" . "\n";
-     echo "<td>". $row['study'] . "</td>" . "\n";
-     echo "</tr>";
+   echo "<td>". $row['fname']. " ". $row['lname'] . "</td>";
+     echo "<td>". $row['course'] . "</td>";
+     echo "<td>". $row['accom'] . "</td>";
+     echo "<td>". $row['study'] . "</td>";
+     echo "</tr>"  . "\n";
      // echo ''. "<tr> <td>" . $row['fname'] . " ". $row['lname'] . "</td><td>" . $row['course'] . "</td><td>" . $row['accom'] . "</td><td>" . $row['study'] . "</td></tr>" . "\n";
   }
   echo "</table>";
