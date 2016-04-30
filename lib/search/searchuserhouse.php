f9a0db0be29af7039c359c75a5f75a0c90c7e9b4
stew1995 <up703451@myport.ac.uk>
2016-04-30 18:09:39 +0100

    Update searchuserhouse.php

--- lib/search/searchuserhouse.php
+++ lib/search/searchuserhouse.php
@@ -11,7 +11,6 @@ if(!$conn) {
   die("Connection to database failed: ". mysqli_connect_error());
 }if(isset($_GET['search']) && $_GET['search'] != '') {
   $search = $_GET['search'];
-  $smoker = $_GET['']
   $result = mysqli_query($conn, "SELECT fname, lname, gender, course, accom, study
                                 FROM user
                                 INNER JOIN uni
