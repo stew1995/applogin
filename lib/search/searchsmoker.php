925f80583b9e999aff992f418e6acfe0c1099af3
stew1995 <up703451@myport.ac.uk>
2016-04-30 16:57:56 +0100

    Update searchsmoker.php

--- lib/search/searchsmoker.php
+++ lib/search/searchsmoker.php
@@ -11,11 +11,11 @@ if(!$conn) {
   die("Connection to database failed: ". mysqli_connect_error());
 }if(isset($_GET['search']) && $_GET['search'] != '') {
   $search = $_GET['search'];
-  $result = mysqli_query($conn, "SELECT fname, lname, gender, course, accom, study, smoker
+  $result = mysqli_query($conn, "SELECT fname, lname, gender, course, accom, study
                                 FROM user
                                 INNER JOIN uni
                                 ON user.id = uni.user_id
-                                WHERE smoker = 'TRUE'
+                                WHERE smoker = 1
                                 AND fname LIKE ('%".$search."%')");
   echo "<table id='searchresulttable'";
     echo "<tr>";
@@ -24,7 +24,6 @@ if(!$conn) {
       echo "<th>Course</th>";
       echo "<th>Accomodation</th>";
       echo "<th>Study Year</th>";
-      echo "<th>Smoker</th>";
     echo "</tr>";
   while($row = mysqli_fetch_array($result)) {
     echo ''. "<tr>";
