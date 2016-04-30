a1249b5b6bfec2bcdea952b81f70da6ebc49dab9
stew1995 <up703451@myport.ac.uk>
2016-04-30 20:52:01 +0100

    Update registerInsert.php

--- lib/registerInsert.php
+++ lib/registerInsert.php
@@ -12,7 +12,7 @@ if(!$conn) {
 
 //INSERT INTO user
 if(!empty($_POST['checkValidation'])){
-  validate_data($data) {
+  function validate_data($data) {
     $data = trim($data);
     $data = stripslashes($data);
     $data = strip_tags($data);
