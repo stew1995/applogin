5eb89d5371f8a6d7c0e0f41710e5a2ec28ef736c
stew1995 <up703451@myport.ac.uk>
2016-04-30 16:44:07 +0100

    Update searchhall.php

--- lib/search/searchhall.php
+++ lib/search/searchhall.php
@@ -11,7 +11,7 @@ if(!$conn) {
   die("Connection to database failed: ". mysqli_connect_error());
 }if(isset($_GET['search']) && $_GET['search'] != '') {
   $search = $_GET['search'];
-  $result = mysqli_query("SELECT * FROM hall WHERE name LIKE ('%".$search."%')");
+  $result = mysqli_query($conn, "SELECT * FROM hall WHERE name LIKE ('%".$search."%')");
     echo "<table id='searchresulttable'";
       echo "<tr>";
         echo "<th>Name</th>";
