f915cc98193e6b8649248f955c658b81db94254e
stew1995 <up703451@myport.ac.uk>
2016-04-30 16:36:03 +0100

    Update searchexec.php
    added gender

--- lib/searchexec.php
+++ lib/searchexec.php
@@ -11,12 +11,12 @@ if(!$conn) {
   die("Connection to database failed: ". mysqli_connect_error());
 }if(isset($_GET['search']) && $_GET['search'] != '') {
   $search = $_GET['search'];
-  
-  //Need to use if statements for the multisearch, with the last being this if nothing is selected, look at rest of search code 
-  //Do statement for study yeart 
-//  $studyyearsql = "SELECT fname, lname, course, study 
-//                  FROM user 
- //                 INNER JOIN uni 
+
+  //Need to use if statements for the multisearch, with the last being this if nothing is selected, look at rest of search code
+  //Do statement for study yeart
+//  $studyyearsql = "SELECT fname, lname, course, study
+//                  FROM user
+ //                 INNER JOIN uni
 //                  ON user.id = uni.user_id
 //                  WHERE fname LIKE ('%".$search."%')
 //                  AND study LIKE ";
@@ -38,10 +38,8 @@ if(!$conn) {
   //  }
   //echo "</table>";
   //$result = mysqli_query($conn, "SELECT * FROM user WHERE fname LIKE ('%".$search."%')");
-  
-  //Halls search needed 
-  //$hallsql = "SELECT * FROM hall WHERE name LIKE ('%".$search."%')";
-  
+
+
   //echo "<table id='searchresulttable'";
   //  echo "<tr>";
     //  echo "<th>Name</th>";
@@ -54,30 +52,32 @@ if(!$conn) {
   //  echo "</tr>";
   //  }
   //echo "</table>";
-  
-  //group search 
-  //smoker true or false 
-  //about yourself questions 
+
+  //group search
+  //smoker true or false
+  //about yourself questions
   //else {
-  $result = mysqli_query($conn, "SELECT fname, lname, course, accom, study 
-                                FROM user  
-                                INNER JOIN uni  
-                                ON user.id = uni.user_id 
+  $result = mysqli_query($conn, "SELECT fname, lname, gender, course, accom, study
+                                FROM user
+                                INNER JOIN uni
+                                ON user.id = uni.user_id
                                 WHERE fname LIKE ('%".$search."%')");
   echo "<table id='searchresulttable'";
     echo "<tr>";
-     echo "<th>Name</th>";
-     echo "<th>Course</th>";
-     echo "<th>Accomodation</th>";
-    echo "<th>Study Year</th>";
+      echo "<th>Name</th>";
+      echo "<th>Gender</th>";
+      echo "<th>Course</th>";
+      echo "<th>Accomodation</th>";
+      echo "<th>Study Year</th>";
     echo "</tr>";
   while($row = mysqli_fetch_array($result)) {
-     echo ''. "<tr>";
+    echo ''. "<tr>";
       echo "<td>". $row['fname']. " ". $row['lname'] . "</td>";
-       echo "<td>". $row['course'] . "</td>";
-       echo "<td>". $row['accom'] . "</td>";
+      echo "<td>". $row['gender'] . "</td>";
+      echo "<td>". $row['course'] . "</td>";
+      echo "<td>". $row['accom'] . "</td>";
       echo "<td>". $row['study'] . "</td>";
-     echo "</tr>";
+    echo "</tr>";
   }
   echo "</table>" . "\n";
 }
