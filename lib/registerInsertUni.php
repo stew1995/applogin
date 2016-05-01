09fe0e800f866b5309e44dd3206c07a332827be0
stew1995 <up703451@myport.ac.uk>
2016-05-01 16:53:33 +0100

    Update registerInsertUni.php

--- lib/registerInsertUni.php
+++ lib/registerInsertUni.php
@@ -32,7 +32,7 @@ if(isset($_POST['submitUniInfoBtn'])){ // needs new button
     if(!$userCheck) {
       echo "Could not find email linked to ID in database" . mysqli_error($conn);
     } while ($row = mysqli_fetch_assoc($userCheck)){
-        $row['id']
+        $row['id'];
     } else {
         $sqluni =
         "INSERT INTO uni (user_id, course, study, accom)
