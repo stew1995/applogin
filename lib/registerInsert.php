eb00a85a8d7f6b0b3fef3015deaa4f034b347343
stew1995 <up703451@myport.ac.uk>
2016-05-01 14:16:49 +0100

    Update registerInsert.php

--- lib/registerInsert.php
+++ lib/registerInsert.php
@@ -44,7 +44,7 @@ if(!$conn) {
   //Check if user registered data has been inputted correctly
   $check = mysqli_query($conn, $sqlregister);
   if(!$check) {
-    echo "Data was not inserted" . mysql_error($conn); exit
+    echo "Data was not inserted" . mysql_error($conn); 
   } else {
     echo "Data entered successfully"; exit
   }
