4f0c3d23795ad19c67cd53a2feb4964a60b20d51
stew1995 <up703451@myport.ac.uk>
2016-05-01 18:07:28 +0100

    Update editProfile.php

--- lib/editProfile.php
+++ lib/editProfile.php
@@ -27,12 +27,12 @@ if(!empty($startingEmail) && ($startingPass)) {
     $lname = validate_data($_POST['profileLName']);
     if(!empty($fname)) {
       echo $row ['id'];
-      $fnamesql = "UPDATE user SET fname='$fname' WHERE id='[id]'";
+      $fnamesql = "UPDATE user SET fname='$fname' WHERE id='['id']'";
       mysqli_query($conn, $fnamesql) or die("Cannot change first name data in database" . mysqli_error($conn));
 
     } else if (!empty($lname)) {
       echo $row ['id'];
-      $lnamesql = "UPDATE user SET fname='$lname' WHERE id='[id]'";
+      $lnamesql = "UPDATE user SET fname='$lname' WHERE id='['id']'";
       mysqli_query($conn, $lnamesql) or die("Cannot change last nanme data in database" . mysqli_error($conn));
 
   }
@@ -49,7 +49,7 @@ if(!empty($startingEmail) && ($startingPass)) {
       if($firstPass != $retypePass) {
         echo "Passwords dont match";
       } else {
-        $sql = "UPDATE user SET password='$retypePass' WHERE id='[id]'";
+        $sql = "UPDATE user SET password='$retypePass' WHERE id='['id']'";
         mysqli_query($conn, $sql) or die("Cannot change password data in database" . mysqli_error($conn));
 
       }
