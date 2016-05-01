a3e2f43d9ea011c4e635b253c782731f3ea34e28
stew1995 <up703451@myport.ac.uk>
2016-05-01 17:45:05 +0100

    Update editProfile.php

--- lib/editProfile.php
+++ lib/editProfile.php
@@ -8,31 +8,31 @@ function  validate_data($data) {
   $data = htmlspecialchars($data);
   return $data;
 }
+
+        
 //Get the user email for the ID
 $startingEmail = validate_data($_POST['emailID']);
 $startingPass = validate_data($_POST['passwordID']);
 if(!empty($startingEmail) && ($startingPass)) {
   $getID = "SELECT id FROM user WHERE email='$startingEmail' AND password='$startingPass'";
-  if(!mysqli_query($conn, $getID)) {
+  $checkID  = mysqli_query($conn, $getID)
+  if(!$checkID) {
     die("Can not get user ID" . mysqli_error($conn));
-  }  else {
+  } else {
+    $row=mysqli_fetch_assoc($userID);
+    echo $row ['id'];
   //Edit first and last name
   if(isset($_POST['submitName'])) {
     $fname = validate_data($_POST['profileName']);
     $lname = validate_data($_POST['profileLName']);
     if(!empty($fname)) {
-      $fnamesql = "UPDATE user SET fname='$fname' WHERE id='$getID'";
+      $fnamesql = "UPDATE user SET fname='$fname' WHERE id='[id]'";
       mysqli_query($conn, $sql) or die("Cannot change first name data in database" . mysqli_error($conn));
 
     } else if (!empty($lname)) {
-      $lnamesql = "UPDATE user SET fname='$lname' WHERE id='$getID'";
+      $lnamesql = "UPDATE user SET fname='$lname' WHERE id='[id]'";
       mysqli_query($conn, $sql) or die("Cannot change last nanme data in database" . mysqli_error($conn));
 
-    } else if(!empty($lname) && !empty($fname)){
-      $sql = "UPDATE user SET fname='$fname', lname='$lname' WHERE id='$getID'";
-      mysqli_query($conn, $sql) or die("Cannot change first name and last name data in database" . mysqli_error($conn));
-
-    }
   }
 
   //Edit password
@@ -46,7 +46,7 @@ if(!empty($startingEmail) && ($startingPass)) {
       if($firstPass != $retypePass) {
         echo "Passwords dont match";
       } else {
-        $sql = "UPDATE user SET password='$retypePass' WHERE fname='$getID'";
+        $sql = "UPDATE user SET password='$retypePass' WHERE id='[id]'";
         mysqli_query($conn, $sql) or die("Cannot change password data in database" . mysqli_error($conn));
 
       }
@@ -63,7 +63,7 @@ if(!empty($startingEmail) && ($startingPass)) {
       if ($newemail != $retype) {
         echo "Emails do not match ";
       } else {
-        $sql = "UPDATE user SET email='$newemail' WHERE fname='$getID'";
+        $sql = "UPDATE user SET email='$newemail' WHERE id='[id]'";
         mysqli_query($conn, $sql) or die("Cannot change email data in database" . mysqli_error($conn));
 
       }
@@ -73,7 +73,7 @@ if(!empty($startingEmail) && ($startingPass)) {
   if(isset($_POST['submitAddress'])) {
     $address = validate_data($_POST['newaddress']);
     if(!empty($address)){
-      $sql = "UPDATE user SET address='$address' WHERE fname='$getID'";
+      $sql = "UPDATE user SET address='$address' WHERE id='[id]'";
       mysqli_query($conn, $sql) or die("Cannot change address data in database" . mysqli_error($conn));
 
     }
@@ -82,7 +82,7 @@ if(!empty($startingEmail) && ($startingPass)) {
   if(isset($_POST['submitpostcode'])) {
     $postcode = validate_data($_POST['newpostcode']);
     if(!empty($postcode)){
-      $sql = "UPDATE user SET postcode='$postcode' WHERE fname='$getID";
+      $sql = "UPDATE user SET postcode='$postcode' WHERE id='[id]";
       mysqli_query($conn, $sql) or die("Cannot change postcode data in database" . mysqli_error($conn));
 
     }
@@ -93,13 +93,13 @@ if(!empty($startingEmail) && ($startingPass)) {
     $newhome = validate_data($_POST['newhome']);
 
     if(!empty($newmobile)) {
-      $sql = "UPDATE user SET mobile='$newmobile' WHERE fname='$getID'";
+      $sql = "UPDATE user SET mobile='$newmobile' WHERE id='[id]'";
       mysqli_query($conn, $sql) or die("Cannot change mobile data in database" . mysqli_error($conn));
 
     }
 
     if(!empty($newhome)) {
-      $sql = "UPDATE user SET homemobile='$newhome' WHERE fname='$getID'";
+      $sql = "UPDATE user SET homemobile='$newhome' WHERE id='[id]'";
       mysqli_query($conn, $sql) or die("Cannot change home data in database" . mysqli_error($conn));
 
     }
@@ -110,7 +110,7 @@ if(!empty($startingEmail) && ($startingPass)) {
   if(isset($_POST['submithobbie'])) {
     $hobbie =  validate_data($_POST['hobbielist']);
     if(!empty($hobbie)) {
-      $sql ="UPDATE user SET hobbie='$searchID' WHERE fname='$getID'";
+      $sql ="UPDATE user SET hobbie='$searchID' WHERE id='[id]'";
       mysqli_query($conn, $sql) or die("Cannot change hobbie data in database" . mysqli_error($conn));
 
     }
@@ -120,7 +120,7 @@ if(!empty($startingEmail) && ($startingPass)) {
   if(isset($_POST['submitsociety'])) {
     $society = validate_data($_POST['societylist']);
     if(!empty($society)) {
-      $sql = "UPDATE user SET society='$searchID' WHERE fname='$getID'";
+      $sql = "UPDATE user SET society='$searchID' WHERE id='[id]'";
       mysqli_query($conn, $sql) or die("Cannot change society data in database" . mysqli_error($conn));
 
     }
@@ -130,7 +130,7 @@ if(!empty($startingEmail) && ($startingPass)) {
   if(isset($_POST['submityear'])) {
     $year = validate_data($_POST['yearlist']);
     if(!empty($year)){
-      $sql = "UPDATE uni SET study='$year' WHERE fname='$getID'";
+      $sql = "UPDATE uni SET study='$year' WHERE id='[id]'";
       mysqli_query($conn, $sql) or die("Cannot change year data in database" . mysqli_error($conn));
 
     }
@@ -139,7 +139,7 @@ if(!empty($startingEmail) && ($startingPass)) {
   if(isset($_POST['submitcourse'])) {
     $course = validate_data($_POST['courselist']);
     if(!empty($course)) {
-      $sql = "UPDATE uni SET course='$getID' WHERE fname='$getID'";
+      $sql = "UPDATE uni SET course='$getID' WHERE id='[id]'";
       mysqli_query($conn, $sql) or die("Cannot change course data in database" . mysqli_error($conn));
 
     }
@@ -149,7 +149,7 @@ if(!empty($startingEmail) && ($startingPass)) {
   if(isset($_POST['submitaccom'])) {
     $accom = validate_data($_POST['accomlist']);
     if(!empty($accom)) {
-      $sql = "UPDATE uni SET accom='$accom' WHERE fname='$getID'";
+      $sql = "UPDATE uni SET accom='$accom' WHERE id='[id]'";
       mysqli_query($conn, $sql) or die("Cannot change accomodation data in database" . mysqli_error($conn));
 
     }
@@ -160,7 +160,7 @@ if(!empty($startingEmail) && ($startingPass)) {
   if(isset($_POST['submitsmoke'])) {
     $smoke = $_POST['smoke'];
     if(!empty($smoker)) {
-      $sql = "UPDATE user SET smoker='$smoke' WHERE fname='$getID'";
+      $sql = "UPDATE user SET smoker='$smoke' WHERE id='[id]'";
       mysqli_query($conn, $sql) or die("Cannot change smoker data in database" . mysqli_error($conn));
     }
   }
