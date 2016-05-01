e0f586abde48e37996e8e8d0e2ab1fe03511f3aa
stew1995 <up703451@myport.ac.uk>
2016-05-01 14:48:41 +0100

    Update registerInsertUni.php

--- lib/registerInsertUni.php
+++ lib/registerInsertUni.php
@@ -1,16 +1,6 @@
 <?php // CREATE NOT NULL ON UNIQUE KEY
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "web";
-//Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-//Check connection
-if(!$conn) {
-  die("Connection to database failed: ". mysqli_connect_error());
-}
 
-//INSERT INTO user
+//INSERT INTO uni
 if(isset($_POST['submitUniInfo'])){ // needs new button
   if(!empty($_POST['retypeEmail'])) {
     function  validate_data($data) {
@@ -20,6 +10,17 @@ if(isset($_POST['submitUniInfo'])){ // needs new button
       $data = htmlspecialchars($data);
       return $data;
     }
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "web";
+    //Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    //Check connection
+    if(!$conn) {
+      die("Connection to database failed: ". mysqli_connect_error());
+    }
+
     $email = validate_data($_POST['retypeEmail']);
     $course = $_POST['courseList'];
     $studyYear = $_POST['yearList'];
@@ -29,14 +30,19 @@ if(isset($_POST['submitUniInfo'])){ // needs new button
     $userID = "SELECT id FROM user WHERE email ='$email'";
     $userCheck = mysqli_query($conn, $userID);
     if(!$userCheck) {
-      die("Could not find email linked to ID in database" . mysqli_error($conn));
+      echo "Could not find email linked to ID in database" . mysqli_error($conn);
     } else {
         $sqluni =
         "INSERT INTO uni (user_id, course, study, accom)
-        VALUES ('$userCheck','$course', '$studyYear', '$accom')";
+        VALUES ('$userID','$course', '$studyYear', '$accom')";
 
         //Check if user data entered is correct
-        mysqli_query($conn, $sqluni) or die(mysqli_error($conn));
+          $check = mysqli_query($conn, $sqluni);
+            if(!$check) {
+              echo "Data was not inserted" . mysql_error($conn);
+            } else {
+              echo "Data entered successfully";
+            }
       }
     }
 }
