0dfc28d5354b909dd3832e2a4f304112f5aeac13
stew1995 <up703451@myport.ac.uk>
2016-05-01 16:54:16 +0100

    Update registerInsertUni.php

--- lib/registerInsertUni.php
+++ lib/registerInsertUni.php
@@ -36,7 +36,7 @@ if(isset($_POST['submitUniInfoBtn'])){ // needs new button
     } else {
         $sqluni =
         "INSERT INTO uni (user_id, course, study, accom)
-        VALUES ($row['id'],'$course', '$studyYear', '$accom')";
+        VALUES ('$userCheck','$course', '$studyYear', '$accom')";
 
         //Check if user data entered is correct
           $check = mysqli_query($conn, $sqluni);
