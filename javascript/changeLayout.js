fc10f772edde897ff7b4772618e54b60c8f51496
stew1995 <up703451@myport.ac.uk>
2016-05-01 18:31:57 +0100

    Update changeLayout.js

--- javascript/changeLayout.js
+++ javascript/changeLayout.js
@@ -43,58 +43,40 @@ function changeLayout(){
   var el = 'pages/', source;
   if (housesection1 === true){
     el += 'housesidebar.html';
-    source = sidesource;
   }else if (housesection2 === true){
     el += 'housecenter.html';
-    source = centersource;
   } else if (housesection3 === true){
     el += 'houserightbar.html';
-    source = rightsource;
   } else if (hallsection1 === true){
     el += 'hallssidebar.html';
-    source = sidesource;
   } else if (hallsection2 === true){
     el += 'hallscenter.html';
-    source = centersource;
   } else if (hallsection3 === true){
     el += 'hallsrightbar.html';
-    source = rightsource;
   } else if (profilesection1 === true){
     el += 'profilesidebar.html';
-    source = sidesource;
   } else if (profilesection2 === true){
     el += 'profilecenter.html';
-    source = centersource;
   } else if (profilesection3 === true){
     el += 'profilerightbar.html';
-    source = rightsource;
   } else if (socialsection1 === true){
     el += 'socialsidebar.html';
-    source = sidesource;
   } else if (socialsection2 === true){
     el += 'socialcenter.html';
-    source = centersource;
   } else if (socialsection3 === true){
     el += 'socialrightbar.html';
-    source = rightsource;
   } else if (supportsection1 === true){
     el += 'supportsidebar.html';
-    source = sidesource;
   } else if (supportsection2 === true){
     el += 'supportcenter.html';
-    source = centersource;
   } else if (supportsection3 === true){
     el += 'supportrightbar.html';
-    source = rightsource;
   } else if (mapssection1 === true) {
     el += 'mapsidebar.html';
-    source = sidesource;
   } else if (mapsection2 === true) {
     el += 'mapcenter.html';
-    source = centersource;
   } else if (mapsection3 === true) {
     el += 'maprightbar.html';
-    source = rightsource;
   }
 
 
@@ -110,7 +92,7 @@ function changeLayout(){
   if (el && el !== 'pages/' && loc && source){
     ajaxGet(el, function(data){
       var existing = document.getElementById(loc).innerHTML;
-      document.getElementById(source).innerHTML = data;
+      document.getElementById(source).innerHTML = existing;
       document.getElementById(loc).innerHTML = data;
     });
   }
