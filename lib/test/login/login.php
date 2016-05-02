aff30705294c1187d39150cffab533f6bf08787c
stew1995 <up703451@myport.ac.uk>
2016-05-02 11:06:02 +0100

    Update and rename login.php to lib/test/login/login.php

--- lib/test/login/login.php
+++ lib/test/login/login.php
@@ -3,32 +3,32 @@
 session_start(); // Starting Session
 $error=''; // Variable To Store Error Message
 if (isset($_POST['login'])) {
-if (empty($_POST['email']) || empty($_POST['password'])) {
+if (empty($_POST['username']) || empty($_POST['password'])) {
 $error = "Username or Password is invalid";
 }
 else
 {
 // Define $username and $password
-$email=$_POST['email'];
+$username=$_POST['username'];
 $password=$_POST['password'];
 $servername = "localhost";
-$email = "root";
+$username = "root";
 $password = "root";
 $dbname = "web";
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
 $connection = mysql_connect($servername, $email, $password);
 // To protect MySQL injection for Security purpose
-$email = stripslashes($email);
+$username = stripslashes($username);
 $password = stripslashes($password);
-$email = mysql_real_escape_string($email);
+$username = mysql_real_escape_string($username);
 $password = mysql_real_escape_string($password);
 // Selecting Database
 $db = mysql_select_db($dbname, $connection);
 // SQL query to fetch information of registerd users and finds user match.
-$query = mysql_query("select * from user where password='$password' AND email='$email'", $connection);
+$query = mysql_query("select * from user where password='$password' AND email='$username'", $connection);
 $rows = mysql_num_rows($query);
 if ($rows == 1) {
-$_SESSION['login_user']=$email; // Initializing Session
+$_SESSION['login_user']=$username; // Initializing Session
 header("location: index.php"); // Redirecting To Other Page
 } else {
 $error = "Username or Password is invalid";
